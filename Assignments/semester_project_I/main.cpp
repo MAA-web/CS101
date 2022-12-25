cbfbfddc3c58523f2d16a0bd69f5f7f623133d0e
blissedswan <blissedswan@penryn.conroe>
2022-12-26 02:14:12 +0500

    completed first semester project just have to put in the file reading and rand()

--- Assignments/semester_project_I/main.cpp
+++ Assignments/semester_project_I/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
 using namespace std;
-/*namespace salted_potato  {
+namespace salted_potato  {
+    int length(char * a) {
+        int len = 0;
+        for (int i = 0; a[i] != '\0' ; i++) {
+            len++;
+        }
+        return len;
+    }
+}
 
-};*/
-int lena{0}, lenb{0};
-
-char * add(const char *,const char *);
+char * add(char *,char *);
 void addEql(char* &a, char* &b);
 char getChar(char * &, int);
 void setChar(char * &, int, char);
@@ -15,22 +20,15 @@ bool isGreater(char *, char *);
 bool isSmaller(char *, char *);
 char * getSubstring(char *, int, int);
 char * shiftLeft(char * & ,int);
-//char *b = static_cast<char*>(malloc(sizeof(char) * 8 ));
+char * shiftRight(char * &, int );
+
 int main() {
+
     char *a = "ABCDEFG";
     char *b = "1234567";
-
-
-    for (int i = 0; a[i] != '\0' ; i++) {
-        lena++;
-    }
-    for (int i = 0; b[i] != '\0' ; i++) {
-        lenb++;
-    }
-
+    /*
     cout<<"add: "<<add(a, b)<<endl;
-    //addEql(a, b);
-    //cout<<"addEql: "<<a<<endl;
+
     cout<<"getChar: "<<getChar(a, 1)<<endl;
     char ch = '1';
     setChar(b,4,ch);
@@ -39,14 +37,163 @@ int main() {
     cout<<"isGreater: "<<isGreater(a,b)<<endl;
     cout<<"isSmaller: "<<isSmaller(a,b)<<endl;
     cout<<"getSubstring: "<<getSubstring(a, 0, 3)<<endl;
-    //char *a = "ABCDEFG";
+
     cout<<"shiftLeft: "<<shiftLeft(a,1)<<endl;
-    cout<<a;
+    cout<<"shiftRight: "<<shiftRight(a, 1)<<endl;
+    cout<<a<<endl;
+    cout<<"shiftRight: "<<shiftRight(a, 3)<<endl;
+    cout<<shiftLeft(a, 3)<<endl;
+    cout<<a<<endl;
+    */
+
+
+
+
+    cout<<"|--------------------------------------------------------------------------Task two--------------------------------------------------------------------------|"<<endl;
+    char * c = "china";
+    int length = salted_potato::length(c);
+    char * check = static_cast<char * >(malloc(sizeof(char) * length));
+    check = "_____";
+    char * check1 = static_cast<char * >(malloc(sizeof(char) * 11));
+    check1 = "___________";
+    int mistakes = 0;
+    int right = 0;
+    char w = ' ';
+    while(mistakes <= 11) {
+
+        while(true){
+            cout<<"Type a word (tries left "<<11-mistakes<<" ): ";
+            cin>>w;
+
+            int h = 0;
+            for (int i = 0; i < 11; i++) {
+                if (check1[i] == w){
+                    h++;
+                    continue;
+                }
+                else if (check1[i] == '_' && h == 0) {
+                    setChar(check1,i,w);
+                    break;
+                }
+            }
+
+            for (int i = 0; i < length; i++) {
+                if(check[i] == w) {
+                    h++;
+                    continue;
+                }
+            }
+            if (h == 0){
+                break;
+            }
+            else {
+                cout<<"Word already used! choose another one"<<endl;
+            }
+        }
+
+        /*printing*/
+        int g = 0;
+        for (int i = 0; i < length; i++) {
+            if(c[i] == w) {
+                setChar(check,i,w);
+                g++;
+                right++;
+            }
+        }
+        if(g == 0){mistakes++;}
+
+        if(system("cls"))system("clear");
+
+        if(mistakes == 11) {
+            cout << " (¨!¨)\n"
+                    "  /|\\\n"
+                    "   |\n"
+                    "  / \\\n ";
+            cout<<"YOU LOSE!"<<endl;
+            cout<<"The word was "<<c<<endl;
+            break;
+        }
+        if(mistakes == 10){
+            cout << " (¨!¨)\n"
+                    "  /|\\\n"
+                    "   |\n"
+                    "   \\\n ";
+        }
+        if(mistakes == 9){
+            cout << " (¨!¨)\n"
+                    "  /|\\\n"
+                    "   |\n"
+                    "  ";
+        }
+        if(mistakes == 8){
+            cout << " (¨!¨)\n"
+                    "  /|\\\n"
+                    "   "
+                    " ";
+        }
+        if(mistakes == 7){
+            cout << " (¨!¨)\n"
+                    "  /|\n"
+                    "  "
+                    "  ";
+        }
+        if(mistakes == 6){
+            cout << " (¨!¨)\n"
+                    "  /\n"
+                    "  "
+                    "  ";
+        }
+        if(mistakes == 5){
+            cout << " (¨!¨)\n"
+                    "  "
+                    "  "
+                    "  ";
+        }
+        if(mistakes == 4){
+            cout << " (¨!)\n"
+                    "  "
+                    "  "
+                    "  ";
+        }
+        if(mistakes == 3){
+            cout << " (¨)\n"
+                    "  "
+                    "  "
+                    "  ";
+        }
+        if(mistakes == 2){
+            cout << " ()\n"
+                    "  "
+                    "  "
+                    "  ";
+        }
+        if(mistakes == 1){
+            cout << " (\n"
+                    "  "
+                    "  "
+                    "  ";
+        }
+        if(mistakes == 0){
+            cout << "  "
+                    "  "
+                    "  "
+                    "  ";
+        }
+        cout<<endl;
+        cout<<check<<endl;
+        if (right == length){
+            cout<<"YOU WIN"<<endl;
+            cout<<"The word was "<<c<<endl;
+            break;
+        }
+    }
 
     return 0;
 }
 
-char * add(const char *a,const char *b) {
+char * add(char *a,char *b) {
+    int lena = salted_potato::length(a);
+    int lenb = salted_potato::length(b);
     char* c = static_cast<char*>(malloc(sizeof(char) * (lena + lenb + 1)));
     for (int i = 0,j=0; j < lenb ,i < lena;j++, i++) {
         c[i] = a[i];
@@ -57,6 +204,8 @@ char * add(const char *a,const char *b) {
 }
 
 void addEql(char * &a, char * &b) {
+    int lena = salted_potato::length(a);
+    int lenb = salted_potato::length(b);
     char* c = static_cast<char*>(malloc(sizeof(char) * (lena + lenb + 1)));
     for (int i = 0 ; i < lena ; i++)
         c[i] = *(a + (i * sizeof(char)));
@@ -72,6 +221,7 @@ char getChar(char * &a, int i){
 }
 
 void setChar(char * &b, int i, char ch){
+    int lenb = salted_potato::length(b);
     char * d = static_cast<char*>(malloc(sizeof(char) * lenb));
     for (int j = 0; j < lenb; ++j) {
         if (j == i) d[j] = ch;
@@ -81,16 +231,22 @@ void setChar(char * &b, int i, char ch){
 }
 
 bool isEqual(char *a, char *b) {
+    int lena = salted_potato::length(a);
+    int lenb = salted_potato::length(b);
     if(lena == lenb) return true;
     else return false;
 }
 
 bool isGreater(char *a, char *b) {
+    int lena = salted_potato::length(a);
+    int lenb = salted_potato::length(b);
     if (lena > lenb) return true;
     else return false;
 }
 
 bool isSmaller(char *a, char *b) {
+    int lena = salted_potato::length(a);
+    int lenb = salted_potato::length(b);
     if (lena < lenb) return true;
     else return false;
 }
@@ -104,16 +260,33 @@ char * getSubstring(char *a, int i, int j) {
 }
 
 char * shiftLeft(char * &a ,int i) {
+    int lena = salted_potato::length(a);
     char * c = static_cast<char *>(malloc(i * sizeof(char)));
     char * d = static_cast<char *>(malloc(lena - i + 1 * sizeof(char)));
     for (int j = 0; j < i ; j++) {
         c[j] = a[j];
     }
-    for (int k = 0, j = i; j <= (lena - i); k++, j++) {
+    for (int k = 0, j = i; j <= (lena); k++, j++) {
         d[k] = a[j];
     }
     a = d;
     return c;
 }
 
+char * shiftRight(char * &a, int i) {
+    int lena = salted_potato::length(a);
+    char * c = static_cast<char *>(malloc(i * sizeof(char)));
+    char * d = static_cast<char *>(malloc(lena - i * sizeof(char)));
+    for (int k = 0, j = lena - 1; j >= (lena - i) ; j--, k++) {
+        c[k] = a[j];
+    }
+    for (int j = 0; j < (lena - i); j++) {
+        d[j] = a[j];
+    }
+    a = d;
+    return c;
+}
 /*addEql  get/setChar  isEqual  isGreater   isSmaller  getSubstring  shiftLeft shiftRight*/
+
+//TODO put in file reading and rand funcion
+//TODO clean up code
